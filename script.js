bb7d6c1ab70d8f11ba61b6ace9fd9a5f87a07e84
maileswapna <maileswapna871@gmail.com>
2025-01-28 13:57:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Web app loaded!');
+//console.log('Web app loaded!');
+// External web app: JavaScript to listen to Canvas events from Salesforce
+
+// Listen for a custom event from Salesforce
+window.addEventListener('message', function(event) {
+    if (event.origin === 'https://platform.salesforce.com') {
+        const data = event.data;
+        if (data.type === 'customEvent') {
+            console.log('Received event from Salesforce:', data.payload);
+        }
+    }
+});
+
+// Example of sending an event back to Salesforce
+function sendToSalesforce() {
+    window.parent.postMessage({
+        type: 'customEvent',
+        payload: { key: 'value' }
+    }, 'https://platform.salesforce.com');
+}
+
